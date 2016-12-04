781d0042d25177b549756e6f88045b33a5d95336
hongwei <hongwei@2redbeans.com>
2016-12-04 16:25:59 +0800

    use test util to render content

--- __tests__/todo.snapshot.test.js
+++ __tests__/todo.snapshot.test.js
@@ -1,11 +1,12 @@
 import Todo from '../app/todo';
-import renderer from 'react-test-renderer';
+import TestUtils from "react-addons-test-utils";
 import React from 'react';
 
 test('Todo component renders the todo correctly', () => {
   const todo = { id: 1, done: false, name: 'Buy Milk' };
-  const rendered = renderer.create(
-    <Todo todo={todo} />
+  const renderer = TestUtils.createRenderer();
+  renderer.render(
+      <Todo todo={todo} />
   );
-  expect(rendered.toJSON()).toMatchSnapshot();
+  expect(renderer.getRenderOutput()).toMatchSnapshot();
 });
